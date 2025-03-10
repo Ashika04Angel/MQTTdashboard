55199740d1c98a276ea110bd95bf8315b62e41e9
Ashika04Angel <ashika04angel@gmail.com>
2025-03-10 18:30:30 +0530

    js

---
+++ script.js
@@ -0,0 +1,23 @@
+const brokerAddress = 'mqtt://test.mosquitto.org:8080/';
+const topic= [
+    'home/Solax/BatteryTemperature',
+    'home/Solax/PvVoltage2',
+    'home/Solax/GridPower'
+];
+const client = mqtt.connect(brokerAddress)
+let val, i, test = []
+let dataArray, voltagevalue = "";
+let max = 500;
+var num1,num2,num3 = '';
+
+client.on('connect',()=>{
+    console.log("connected to MQTT broker")
+    client.subscribe(topic,(err) =>{
+        if(!err){
+            console.log("Subscribed to topic:",topic);
+        }
+        else{
+            console.error("Error subscribing to topic",err);
+        }
+    });
+});
