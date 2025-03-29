76d47eda5b6154adcd379b0f8508787b04f4b59a
Ashika04Angel <ashika04angel@gmail.com>
2025-03-29 14:34:12 +0530

    change

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const brokerAddress = 'mqtt://test.mosquitto.org:8080/';
 const topic= [
+    // 'sensors/temperature/livingroom',
     'home/Solax/BatteryTemperature',
     'home/Solax/PvVoltage2',
     'home/Solax/GridPower'
 ];
 const client = mqtt.connect(brokerAddress)
-let val, i, test = []
-let dataArray, voltagevalue = "";
+let val = [];
+let i =[];
+let dataArray = '';
+var num1 ,num2 ,num3 = '';
 let max = 500;
-var num1,num2,num3 = '';
-
+let test = [];
 client.on('connect',()=>{
     console.log("connected to MQTT broker")
     client.subscribe(topic,(err) =>{
@@ -27,7 +29,7 @@ client.on('message',(topic,message,clientID)=>{
 })
 if(topic === 'home/Solax/BatteryTemperature'){
     document.getElementById("clientID").textContent = value
-    num1 = value;
+    num1 = parseFloat(value);
     test.push({battery:num1})
     console.log({battery:num1})
 }
@@ -37,7 +39,7 @@ else if(topic === 'home/Solax/PvVoltage2'){
     console.log(num2)
 }
 else if(topic === 'home/Solax/GridPower'){
-    document.getElementById("property").textContent = value;
+    document.getElementById("property").textContent = JSON.parse(value);
     num3 = value
     console.log(num3)
 }
@@ -46,3 +48,4 @@ var chartDom = document.getElementById('doughnut');
 var myChart = echart.init(chartDom);
 var option;
 const timer = document.getElementById
+
